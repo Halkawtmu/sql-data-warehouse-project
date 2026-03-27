b155f0c980eb149223896cde5456e2b8d83d7c18
Halkawtmu <halkawt.m.mawlood@gmail.com>
2026-03-27 22:08:51 +0300

    Create DataWarehouse and bronze, silver, gold schemas
    This script creates the 'bronze', 'silver', and 'gold' schemas in the 'DataWarehouse' database.

---
+++ scripts/init_database.sql
@@ -0,0 +1,23 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
